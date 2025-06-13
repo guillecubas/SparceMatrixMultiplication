2e2a99ba6204f55302d75fe751299bf9bd47630d
guillecubas <guillermo.cubas101@alu.ulpgc.es>
2025-06-13 23:26:11 +0100

    Initial commit

---
+++ src/main/java/dence/NaiveMultiplication.java
@@ -0,0 +1,17 @@
+package dence;
+
+public class NaiveMultiplication {
+    public static int[][] multiply(int[][] A, int[][] B) {
+        int n = A.length;
+        int[][] result = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    result[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+        return result;
+    }
+}
